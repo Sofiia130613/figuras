a6a548bf558b7fab40da5bf72a4dde4ea1f0e5b1
Sofiia130613 <102312682+Sofiia130613@users.noreply.github.com>
2022-04-04 19:09:11 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+function fcirculo(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("circulo");
+
+}
+
+function frombo(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("rombo");
+}
+
+function frectangulohorizontal(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("rectangulohorizontal");
+}
+
+function frectangulovertical(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("rectangulovertical");
+}
+
+function fimagen(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("imagen");
+}
+
+function arriba(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("subir");
+}
+
+function abajo(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("bajar");
+}
+
+function izquierda(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("izquierda");
+}
+
+function derecha(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("derecha");
+}
+
+function diagonal(){
+    var selector = document.getElementById ("figura");
+    selector.className='';
+    selector.classList.toggle("diagonal");
+}
